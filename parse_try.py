f7f3b7dac348483970cdf5d4c3d012ee4880429c
VictorKatobchyts <karobchitsvictor@gmail.com>
2022-09-13 20:37:37 +0300

    Исправлен возможный баг из-за того, что он искал не в конкретном месте сопадение, а совпадение "в общем", из-за чего, например, ключ"detox drinks" добавлял в новый файл строку с ключом "natural detox drinks"

--- parse_try.py
+++ parse_try.py
@@ -18,7 +18,7 @@ def sorted_keys(l):
         if i not in n:
             n.append(i)
     return n
-with open('C:\\Users\\Sailwork\\Desktop\\parse\\TestData.csv') as csvfile:
+with open('C:\\Users\\Sailwork\\Desktop\\parse\\Parse_csv_files\\TestData.csv') as csvfile:#C:\Users\Sailwork\Desktop\parse\Parse_csv_files\TestData.csv
     csv_read = csv.reader(csvfile, delimiter=';')
     for row in csv_read:
         new_string = re.sub("[\|[\]]",'', row[4])
@@ -37,17 +37,33 @@ with open('C:\\Users\\Sailwork\\Desktop\\parse\\TestData.csv') as csvfile:
     search_key = re.sub("[\|[\]|']","",str(search_key))
     new_search_key = search_key.replace('"',"")#убирает ковычки 
     new_search_key = new_search_key.split(',')#разделяет ключи (на первый ключ - new_search_key[0]() и на второй ключ -new_search_key[1](MOBILE, DESKTOP))
-    print(new_search_key[0])
+    print(str(new_search_key[0]))
 
 with open('out.csv', 'w', newline='') as output_file:
      writer1 = csv.writer(output_file, delimiter=';',quotechar='|', quoting=csv.QUOTE_MINIMAL)
-     with open('C:\\Users\\Sailwork\\Desktop\\parse\\TestData.csv') as csvfile:
+     with open('C:\\Users\\Sailwork\\Desktop\\parse\\Parse_csv_files\\TestData.csv') as csvfile:
          csv_read = csv.reader(csvfile, delimiter=';')
          for row in csv_read:
              if new_search_key[0] in row[4]:#не ищет с ковычками, только внутренние ключи
-                 if new_search_key[1] in row[4]:
+                new_string = re.sub("[\|[\]]",'', row[4])
+                new_string = new_string.split(',')
+                del new_string[0:2]
+                new_string = str(new_string).replace("\\","")#преобразует список в строку и если при предыдущем преобразовании строки в список через сплит случился "баг", что апостроф в ключе заменился на косую черту (\) -  то мы возвращаем все как было удалив апострроф 
+                new_string = new_string.split(',')
+                new_string.pop(1)#удаляет второй ключ(MOBILE или Desktop)
+                new_string = str(new_string).replace("\'s","~")
+                new_string = re.sub("[\[|\]|']","",new_string)
+                new_string = new_string.replace("~", "'s")
+                new_string = new_string.replace("\\","")
+                new_string = new_string.replace("\"","")
+                print(new_string)
+                if new_string == str(new_search_key[0]): #если первый ключ полностью совпадает с ключом из документа
+                    if new_search_key[1] in row[4]:
                     #if дописать условие сравнения длин строк row[4] и того row[4], которое мы получаем при первом прочтении csv-файла и из которого впоследствии извлекаем ключи
-                    writer1.writerow(row)
+                        writer1.writerow(row)
+
+
+
         #new_string = re.sub("[\|[\]]","", csv_read[4])
         #new_string = new_string.split(',')
         #del new_string[0:2]
---
+++ processing algorithm.py
@@ -0,0 +1,25 @@
+import csv
+import re
+
+with open('C:\\Users\\Sailwork\\Desktop\\parse\\Parse_csv_files\\TestData.csv') as csvfile:
+    csv_read = csv.reader(csvfile, delimiter=';')
+    a=0
+    for row in csv_read:
+        a +=1
+        if (a == 2):
+            print(row[4])
+            new_string = re.sub("[\|[\]]",'', row[4])
+            print(new_string)
+            new_string = new_string.split(',')
+            print(new_string)
+            del new_string[0:2]
+            new_string = str(new_string).replace("\\","")#преобразует список в строку и если при предыдущем преобразовании строки в список через сплит случился "баг", что апостроф в ключе заменился на косую черту (\) -  то мы возвращаем все как было удалив апострроф 
+            new_string = new_string.split(',')
+            new_string.pop(1)#удаляет второй ключ(MOBILE или Desktop)
+            new_string = str(new_string).replace("\'s","~")
+            #new_string = str(new_string).replace("\\","")
+            new_string = re.sub("[\[|\]|']","",new_string)
+            new_string = new_string.replace("~", "'s")
+            new_string = new_string.replace("\\","")
+            print(new_string)
+            break
